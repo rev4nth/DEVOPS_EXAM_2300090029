1d14e6f43f2c55d4638a40619fc853b29b19bc6e
rev4nth <163738353+rev4nth@users.noreply.github.com>
2025-10-18 10:53:22 +0530

    Update App.jsx

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/Navbar.jsx";
 function App() {
   return (
     <div>
