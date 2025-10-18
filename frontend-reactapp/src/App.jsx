a4b346d7ef0e69cf7850cced43f18050e94a3e38
rev4nth <2300090029@kluniversity.in>
2025-10-18 10:12:01 +0530

    updated app

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/NavBar";
+import './NavBar.css';  // Make sure this matches the CSS filename exactly
 
 function App() {
   return (
