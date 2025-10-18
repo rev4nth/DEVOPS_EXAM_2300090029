c60b3308b96e0f26f5c6317b8d18812f4c04818b
rev4nth <2300090029@kluniversity.in>
2025-10-18 10:18:46 +0530

    updated app

--- frontend-reactapp/src/components/NavBar.jsx
+++ frontend-reactapp/src/components/NavBar.jsx
@@ -1,36 +1,16 @@
 import React from "react";
-import { Link, Routes, Route } from "react-router-dom";
-import ProductList from "./ProductList";
-import AddProduct from "./AddProduct";
-import EditProduct from "./EditProduct";
-import ViewProduct from "./ViewProduct";
-import "./navbar.css";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar"; 
+import './navbar.css';
 
-function Navbar() {
+function App() {
   return (
     <div>
-      {/* Navbar */}
-      <nav className="navbar-custom">
-        <Link className="navbar-brand" to="/">Product API</Link>
-        <div className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/add">Add Product</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/">View Products</Link>
-          </li>
-        </div>
-      </nav>
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/add" element={<AddProduct />} />
-        <Route path="/edit/:id" element={<EditProduct />} />
-        <Route path="/view/:id" element={<ViewProduct />} />
-      </Routes>
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
     </div>
   );
 }
 
-export default Navbar;
+export default App;
