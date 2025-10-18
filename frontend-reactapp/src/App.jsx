ef578fd6e8991975ca4509cdcf9ca6a0a4600338
rev4nth <163738353+rev4nth@users.noreply.github.com>
2025-10-18 12:08:11 +0530

    Fix import name for NavBar component

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/NavBar";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar />
+      <NavBar />
       </BrowserRouter>
     </div>
   );
